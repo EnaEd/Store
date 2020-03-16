601b9b33c195c7b5a26fb5e406bc8c036ea9795a
developer <stephencgregory@gmail.com>
2020-03-16 20:10:38 +0200

    add nLayer

---
+++ Store/Store.BLL/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Store.BLL
+{
+    public class Startup
+    {
+        public static void Init(IServiceCollection services , IConfiguration configuration)
+        {
+
+        }
+    }
+}
---
+++ Store/Store.DAL/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Store.DAL
+{
+    public class Startup
+    {
+        public static void Init(IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
