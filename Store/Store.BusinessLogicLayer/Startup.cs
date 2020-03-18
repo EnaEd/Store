7371ce52d4b0fe4a9c35f07d20fe9a7372f563b2
developer <stephencgregory@gmail.com>
2020-03-18 17:20:26 +0200

    add startups

---
+++ Store/Store.BusinessLogicLayer/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Store.BusinessLogicLayer
+{
+    public class Startup
+    {
+        public static void Init(IServiceCollection services, IConfiguration configuration)
+        {
+            DataAccessLayer.Startup.Init(services, configuration);
+        }
+    }
+}
