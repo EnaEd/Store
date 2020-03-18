50cbf42703963ad49ceee8d4bc8789006b001c3b
developer <stephencgregory@gmail.com>
2020-03-18 17:28:36 +0200

    add ApplicationContext

---
+++ Store/Store.DataAccessLayer/AppContext/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Store.DataAccessLayer.Entities;
+
+namespace Store.DataAccessLayer.AppContext
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
