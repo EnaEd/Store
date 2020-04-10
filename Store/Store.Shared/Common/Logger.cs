13e7db6eb3d2a956ffc93c1da368dd35a4831008
Flippinloud <stephencgregory@gmail.com>
2020-04-10 11:27:00 +0300

    add frendly error message

--- Store/Store.Shared/Common/Logger.cs
+++ Store/Store.Shared/Common/Logger.cs
@@ -26,15 +26,14 @@ namespace Store.Shared.Common
         public void Log<TState>(LogLevel logLevel, EventId eventId,
             TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            if (formatter is null)
+            if (exception is null)
             {
                 return;
             }
 
             lock (_lock)
             {
-                //TODO add more frendly error message
-                File.AppendAllText(_path, formatter(state, exception) + Environment.NewLine);
+                File.AppendAllText(_path, $"{DateTime.Now}:>{formatter(state, exception)}\nSource:{exception?.Source}\n{exception?.GetType().FullName}->{exception?.Message}\n{exception?.StackTrace ?? string.Empty}\n\n");
             }
         }
     }
